a734301ad8569a817f8ccc881c02e55a77ae0d20
Aqib <aqib.ahamad21@gmail.com>
2021-04-01 20:28:11 +0300

    commit

---
+++ personal_expenses/lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+import './transaction.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  final List<Transaction> transactions = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter App'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Container(
+            width: double.infinity,
+            child: Card(
+              child: Text('Chart!'),
+              color: Colors.blue,
+              elevation: 5,
+            ),
+          ),
+          Card(
+            child: Text('LIST OF FX!'),
+            color: Colors.red,
+          ),
+        ],
+      ),
+    );
+  }
+}
